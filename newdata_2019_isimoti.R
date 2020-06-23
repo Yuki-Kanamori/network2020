e1423fbda838d6a87c4badbc5b15e54e58def07a
Yuki-Kanamori <y.kanamory@gmail.com>
2020-06-23 17:44:39 +0900

    write.csv

--- newdata_2019_isimoti.R
+++ newdata_2019_isimoti.R
@@ -1,5 +1,21 @@
 require(tidyverse)
-require(openxlsx)
 
 setwd("/Users/Yuki/Dropbox/TokyoBay_CPUE/nominalCPUE")
-siro = read.xlsx("【シログチのみでも同じ】ｲｼﾓﾁ類、ｼﾛｸﾞﾁ、ﾆﾍﾞ合算標本船データ（ビーム曳（スズキ網大目合い））.xls,x")
+y2019 = read.csv("after2019_isimoti.csv", fileEncoding = "CP932")
+colnames(y2019)
+y2019 = y2019 %>% mutate(lon = 経度 %/% 100 + 経度 %% 100/60, lat = 緯度 %/% 100 + 緯度 %% 100/60, test = lubridate::dmy(年月日)) %>% mutate(year = as.numeric(str_sub(test, 1, 4)), month = as.numeric(str_sub(test, 6, 7)), day = as.numeric(str_sub(test, 9, 10))) %>% dplyr::rename(wt = 水温) %>% select(year, month, day, lon, lat, 回数, CPUE, 全銘柄, 込, species, gear, wt)
+write.csv(y2019, "latest_data.csv", fileEncoding = "CP932")
+
+# check the lon&lat ---------------------------------------------
+require(maps)
+require(mapdata)
+library(ggrepel)
+
+p <- ggplot() + coord_fixed() +
+  xlab("Longitude") + ylab("Latitude")
+world_map <- map_data("world")
+jap <- subset(world_map, world_map$region == "Japan")
+jap_cog <- jap[jap$lat > 35 & jap$lat < 38 & jap$long > 139 & jap$long < 141, ]
+t2 <- p + geom_polygon(data = jap_cog, aes(x=long, y=lat, group=group), colour="gray 50", fill="gray 50")+ coord_map(xlim = c(139.5, 140.3), ylim = c(35, 35.75))
+t2 + geom_point(data = y2019, aes(x = lon, y = lat), shape = 16, size = 1)
+summary(new)
