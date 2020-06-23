ae539a1345bd984da13fba3307568aa81dbbdb01
Yuki-Kanamori <y.kanamory@gmail.com>
2020-06-23 17:52:09 +0900

    checked outlier site

--- newdata_2019_isimoti.R
+++ newdata_2019_isimoti.R
@@ -17,5 +17,34 @@ world_map <- map_data("world")
 jap <- subset(world_map, world_map$region == "Japan")
 jap_cog <- jap[jap$lat > 35 & jap$lat < 38 & jap$long > 139 & jap$long < 141, ]
 t2 <- p + geom_polygon(data = jap_cog, aes(x=long, y=lat, group=group), colour="gray 50", fill="gray 50")+ coord_map(xlim = c(139.5, 140.3), ylim = c(35, 35.75))
-t2 + geom_point(data = y2019, aes(x = lon, y = lat), shape = 16, size = 1)
-summary(new)
+t2 + geom_point(data = y2019, aes(x = lon, y = lat), shape = 16, size = 1)+geom_text()
+summary(y2019)
+
+# remove the data
+new2 = y2019 %>% mutate(lonlat = paste(lon, lat, sep = "_"))
+lab = data.frame(lonlat = unique(new2$lonlat)) %>%distinct(lonlat) %>%  mutate(lab = rep(1:length(unique(new2$lonlat))))
+new2 = merge(new2, lab, by = "lonlat")
+new2 = new2 %>% distinct(new2$lab, .keep_all = T)
+head(new2)
+
+g = ggplot(new2, aes(x = lon, y = lat, label = lab))
+p = geom_point()
+# g+p+geom_text_repel()
+g+p+geom_text()
+# 118, 246, 251, 252, 230
+
+# list for remove
+remove = c("118"," 246", "251", "252", "230")
+r_lab1 = lab %>% filter(lab %in% remove) #==だと引っかけられない，%in%でも151が引っかかってこない
+r_lab2 = lab %>% filter(lab %in% 246)
+r_lab = rbind(r_lab1, r_lab2)
+r_lab
+
+r_data = y2019 %>% mutate(lonlat = paste(lon, lat, sep = "_"))
+mode(r_data$lonlat) #character
+mode(r_lab$lonlat) #numeric
+r_lab$lonlat = as.character(as.factor(r_lab$lonlat))
+# r_data = left_join(r_data, r_lab, by = "lonlat", keep = F)
+r_data = merge(r_data, r_lab) %>% select(-lonlat, -lab)
+write.csv(r_data, "OutlierList_new.csv", fileEncoding = "CP932")
+
